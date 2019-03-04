aa04c0240c9f5e68435a8d871ad1f9e93c9df30e
Maura Gerke <mauragerke@lawn-143-215-125-83.lawn.gatech.edu>
2019-03-04 14:53:47 -0500

    #149 Add get digital object and cleanup

--- app/ArchiveSpaceApi.php
+++ app/ArchiveSpaceApi.php
@@ -20,15 +20,16 @@ class ArchiveSpaceApi {
   }
 
   public function authenticate() {
-    include './api_creds_production.php';
+    include './api_creds_prod.php';
 
     try {
+      // sanitize this?
       $authuri = BASE_URI . '/users/' . USERNAME . '/login?password=' . PASSWORD;
 
       $response = $this->client->request('POST', $authuri, [
         'on_stats' => function (GuzzleHttp\TransferStats $stats) use (&$url) {
           $url = $stats->getEffectiveUri();
-        }]); // sanitize this!
+        }]);
 
       echo "Sending request to " . $url . "\n";
     } catch (GuzzleHttp\Exception\ServerException $e) {
@@ -36,48 +37,70 @@ class ArchiveSpaceApi {
     } catch (GuzzleHttp\Exception\ClientException $e) {
       echo "There was a problem with your request.\n";
       echo 'HTTP request URL: ' . $url . "\n";
-      //echo 'HTTP request: ' . $e->getRequest() . "\n";
       echo 'HTTP response status: ' . $e->getResponse()->getStatusCode() . "\n";
-      //echo 'HTTP response: ' . $e->getResponse()->getBody(true) . "\n";
       echo 'Exception: ' . $e->getMessage() . "\n";
-      die(); // maybe don't do this?
+      die(); // use retry with recurs limit
     }
     if ($response->getStatusCode() == 200) {
       echo "Successfully authenticated!\n";
       $data = json_decode($response->getBody(), true);
       $this->session_id = $data['session']; // make a test for this      
       echo "Session id " . $this->session_id . " saved.\n";
-      //var_dump($data);
     } else {
       throw new Error("Something went wrong with your request. Unable to authenticate.\n");
     }
   }
 
   public function getResource(int $repo_id, int $record_id) {
-    //$url = BASE_URI . '/repositories/' . $repo_id . '/resources/' . $record_id;
-    $url = BASE_URI . '/repositories/' . $repo_id . '/resources?all_ids=true';
+    $url = BASE_URI . '/repositories/' . $repo_id . '/resources/' . $record_id;
+    //$url = BASE_URI . '/repositories/' . $repo_id . '/resources?all_ids=true';
 
     try {
       $response = $this->client->request('GET', $url, [
         'headers' => ['X-ArchivesSpace-Session' => $this->session_id],
         'on_stats' => function (GuzzleHttp\TransferStats $stats) use (&$url) {
           $url = $stats->getEffectiveUri();
-        }]); // sanitize this!
+        }]);
     } catch (GuzzleHttp\Exception\ClientException $e) {
-      var_dump($response);
       echo "There was a problem with your request.\n";
       echo 'HTTP request URL: ' . $url . "\n";
       echo 'HTTP response status: ' . $e->getResponse()->getStatusCode() . "\n";
       echo 'Exception: ' . $e->getMessage() . "\n";
-      die(); // maybe don't do this?
+      die(); // make this a retry with a recurs limit
     }
 
     if ($response->getStatusCode() == 200) {
-      echo "Successfully retrieved record!\n";
+      echo "Successfully retrieved resource!\n";
       $data = json_decode($response->getBody(), true);
       var_dump($data);
     } else {
-      throw new Error("Something went wrong with your request. Unable to get record.\n");
+      throw new Error("Something went wrong with your request. Unable to get resource.\n");
+    }
+  }
+
+  public function getDigitalObject(int $repo_id, int $digitalobject_id) {
+    $url = BASE_URI . '/repositories/' . $repo_id . '/digital_objects/' . $digitalobject_id;
+
+    try {
+      $response = $this->client->request('GET', $url, [
+        'headers' => ['X-ArchivesSpace-Session' => $this->session_id],
+        'on_stats' => function (GuzzleHttp\TransferStats $stats) use (&$url) {
+          $url = $stats->getEffectiveUri();
+        }]);
+    } catch (GuzzleHttp\Exception\ClientException $e) {
+      echo "There was a problem with your request.\n";
+      echo 'HTTP request URL: ' . $url . "\n";
+      echo 'HTTP response status: ' . $e->getResponse()->getStatusCode() . "\n";
+      echo 'Exception: ' . $e->getMessage() . "\n";
+      die(); // make this a retry with a recurs limit
+    }
+
+    if ($response->getStatusCode() == 200) {
+      echo "Successfully retrieved digital object!\n";
+      $data = json_decode($response->getBody(), true);
+      var_dump($data);
+    } else {
+      throw new Error("Something went wrong with your request. Unable to get digital object.\n");
     }
   }
 
@@ -86,6 +109,6 @@ class ArchiveSpaceApi {
   echo "TESTING START!\n";
   $c = new ArchiveSpaceApi();
   $c->authenticate();
-  $c->getResource(2, 1752);
+  $c->getDigitalObject(2, 5080);
   echo "TEST DONE!\n";
 ?>
